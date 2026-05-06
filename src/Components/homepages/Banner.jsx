322716f02ad462c2d4556dcc5593ad42661cf24e
mdmahediTech <mehedihasan51020510@gmail.com>
2026-05-05 22:05:24 -0700

    banner sections styled as figma

--- src/Components/homepages/Banner.jsx
+++ src/Components/homepages/Banner.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='container mx-auto h-40 bg-green-950 text-white text-center'>
-            BANNER WILL BE  HERE
+        <div className='max-w-[1200px] mx-auto text-center space-y-4 py-10'>
+           <h1 className='text-4xl font-bold'>Friends to keep close in your life</h1>
+           <p className='text-gray-800'>Your personal shelf of meaningful connections. Browse, tend, and nurture the
+                <br />relationships that matter most.</p>
+                <button className='btn btn-ghost bg-green-950 text-white rounded-sm'> + Add a friend</button>
         </div>
     );
 };
--- src/Components/homepages/Cards.jsx
+++ src/Components/homepages/Cards.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Cards = () => {
     return (
-        <div className='container mx-auto h-20 bg-blue-950 text-white text-center'>
-            <h1>4 cards here</h1>
+        <div className='max-w-[960px] mx-auto text-center space-y-4 py-10'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+                <div className='bg-gray-100 py-10 px-2 shadow rounded-sm'>
+                    <p className='font-bold text-2xl'>10</p>
+                    <h1 className='text-gray-800 font-semibold'>total friend</h1>
+                </div>
+                <div className='bg-gray-100 py-10 px-2 shadow rounded-sm'>
+                    <p className='font-bold text-2xl'>3</p>
+                    <h1 className='text-gray-800 font-semibold'>on track</h1>
+                </div>
+                <div className='bg-gray-100 py-10 px-2 shadow rounded-sm'>
+                    <p className='font-bold text-2xl'>6</p>
+                    <h1 className='text-gray-800 font-semibold'>Need Attentions</h1>
+                </div>
+                <div className='bg-gray-100 py-10 px-2 shadow rounded-sm'>
+                    <p className='font-bold text-2xl'>12</p>
+                    <h1 className='text-gray-800 font-semibold'>interaction this month</h1>
+                </div>
+                
+            </div>
         </div>
     );
 };
